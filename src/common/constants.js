5ff4823221dd293975653cc707b1c8aae632fb2e
Kjell-Arne(Sovak) <kjell-arne@neshagen.no>
2024-02-01 09:57:25 +0100

    Added Constants file for common use

---
+++ src/common/constants.js
@@ -0,0 +1,4 @@
+const BASE_URL = 'https://api.noroff.dev/api/v1/social';
+export const REGISTER_URL = `${BASE_URL}/auth/register`;
+export const LOGIN_URL = `${BASE_URL}/auth/login`;
+export const POST_URL = `${BASE_URL}/posts`;
